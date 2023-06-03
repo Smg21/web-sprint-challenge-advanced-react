010c46ce9cadfd920f411baf31d35abaaca4376f
Sabrina <Smg21022@gmail.com>
2023-06-03 10:04:36 -0700

    got coordinates showing up

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+
+import React, { useState, useEffect } from 'react'
 
 export default function AppFunctional(props) {
 
@@ -6,6 +7,7 @@ const [initialMessage, setInitialMessage] = useState('')
 const [initialEmail, setInitialEmail] = useState('')
 const [initialSteps, setInitialSteps] = useState(0)
 const [currentIndex, setCurrentIndex] = useState(4)
+const [currentCoordinate, setCurrentCoordinate] = useState()
 
   const grid = [
     [1,1],[1,2],[1,3],
@@ -20,6 +22,8 @@ const [currentIndex, setCurrentIndex] = useState(4)
     down: (index) => index + 3
   }
 
+
+
   function getXY(grid,idx) {
     const coordinatess = grid.find((block,index) => {
       
@@ -29,11 +33,16 @@ const [currentIndex, setCurrentIndex] = useState(4)
     })
       return coordinatess;
   }
-  const coordinate = getXY(grid, 3);
+
+  useEffect(()=> {
+    setCurrentCoordinate(getXY(grid, currentIndex))
+  }, [currentIndex])
+  
  
-  function getXYMessage(grid) {  
-    return  `Cordinates (${coordinate})`;
-  }
+  // function getXYMessage(grid, currentIndex) {  
+  //   const coordinate = getXY(grid, currentIndex);
+  //   return  `Cordinates (${coordinate})`;
+  // }
 
   function reset() {
     setInitialEmail('');
@@ -42,10 +51,9 @@ const [currentIndex, setCurrentIndex] = useState(4)
     setInitialIndex(4);
   }
 
-  function getNextIndex(direction, grid, e) {
+  function getNextIndex(direction, grid, e, currentIndex) {
+    let coordinate = getXY(grid, currentIndex);
     let userDirection = e.target.id 
-    
-    const coordinate = getXY(grid, currentIndex)
     let currentIndexLocation = grid.reduce((acc, block,index)  =>{ 
       if (coordinate === block){
         let data = {index:index, block:block}
@@ -54,27 +62,31 @@ const [currentIndex, setCurrentIndex] = useState(4)
      return acc; 
   },{});
       const nextIndex= direction[userDirection](currentIndexLocation.index)
+      setCurrentCoordinate(currentIndexLocation.block)
       return nextIndex;
+    
   }
 
-  function move(buttonDirection, grid, e) {
+  function move(buttonDirection, grid, e, currentIndex) {
 if (e.target.id === 'up'){
-    setCurrentIndex(getNextIndex(buttonDirection, grid, e));
+    setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
     onChange(e);
 } else if (e.target.id === 'down'){
-   setCurrentIndex(getNextIndex(buttonDirection, grid, e));
+   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
    onChange(e);
 } else if (e.target.id === 'left'){
-   setCurrentIndex(getNextIndex(buttonDirection, grid, e));
+   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
    onChange(e);
 } else if (e.target.id === 'right'){
-  setCurrentIndex(getNextIndex(buttonDirection, grid, e));
+  setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
   onChange(e);
 }
   }
   function onChange(evt) {
     if (evt) {
       setInitialSteps((prev) => prev+ 1)
+      setCurrentCoordinate(getXY(grid, currentIndex))
+      console.log(`line 82`, currentCoordinate)
     }
    
   }
@@ -86,7 +98,7 @@ if (e.target.id === 'up'){
   return (
     <div  id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
+        <h3 id="coordinates">{currentIndex === 4 ?  `Coordinates ${currentCoordinate}`: `Coordinates ${currentCoordinate}`}</h3>
         <h3 id="steps">You moved {initialSteps} times</h3>
       </div>
       <div id="grid">
@@ -102,10 +114,10 @@ if (e.target.id === 'up'){
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button onClick={(e) => move(buttonDirection, grid, e)} id="left">LEFT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e)} id="up">UP</button>
-        <button onClick={(e) => move(buttonDirection, grid, e)} id="right">RIGHT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e)} id="down">DOWN</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="left">LEFT</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="up">UP</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="right">RIGHT</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="down">DOWN</button>
         <button id="reset">reset</button>
       </div>
       <form>
@@ -115,3 +127,8 @@ if (e.target.id === 'up'){
     </div>
   )
 }
+
+
+
+
+
