c09c49065b4ca803a8901830ad324f5dbefbff15
Sabrina <Smg21022@gmail.com>
2023-06-30 09:03:28 -0700

    grid no longer reverses when clicking left

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -19,19 +19,21 @@ const [currentCoordinate, setCurrentCoordinate] = useState()
 
   //Setting which direction each button will move on the array above
  
-  const buttonDirection = {
-    left: (index) => index - 1,
-    right: (index) => index + 1,
-    up: (index) => index -3,
-    down: (index) => index + 3
-  }
+  // const buttonDirection = {
+  //   left: (index) => index - 1,
+  //   right: (index) => index + 1,
+  //   up: (index) => index -3,
+  //   down: (index) => index + 3
+  // }
+
+  
 
   
   // const buttonDirection = {
-  //   left: (index % 3 === 0) ? index : index - 1,
-  //   right: ((index -2) % 3 === 0) ? index : index + 1,
-  //   up: (index < 3) ? index :  index -3,
-  //   down: (index > 5) ? index : index + 3
+  //   left: (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1,
+  //   right: ((currentIndex -2) % 3 === 0) ? currentIndex : currentIndex + 1,
+  //   up: (currentIndex < 3) ? currentIndex :  currentIndex -3,
+  //   down: (currentIndex > 5) ? currentIndex : currentIndex + 3
   // }
 
   //For now I want to find a way to prevent the square from going back/reversed. This code is not working as it says index is not defined, and I tried Currentindex as well. 
@@ -70,39 +72,39 @@ const [currentCoordinate, setCurrentCoordinate] = useState()
 
   //to let the computer know where the current location is 
 
-  function getNextIndex(direction, grid, e, currentIndex) {
-    let coordinate = getXY(grid, currentIndex);
-    let userDirection = e.target.id 
-    let currentIndexLocation = grid.reduce((acc, block,index)  =>{ 
-      if (coordinate === block){
-        let data = {index:index, block:block}
-      return data;
-      }
-     return acc; 
-  },{});
-      const nextIndex= direction[userDirection](currentIndexLocation.index)
-      setCurrentCoordinate(currentIndexLocation.block)
-      return nextIndex;
+  function getNextIndex(direction) {
+    switch (direction) {
+      case 'up':
+        return (currentIndex < 3) ? currentIndex : currentIndex - 3
+      case 'down':
+        return (currentIndex > 5) ?currentIndex : currentIndex + 3
+      case 'left':
+        return (currentIndex % 3 === 0) ? currentIndex : currentIndex- 1
+      case 'right':
+        return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1
+    }
+    
     
   }
   
 
 
-  function move(buttonDirection, grid, e, currentIndex) {
-if (e.target.id === 'up'){
-    setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
-    onChange(e);
-} else if (e.target.id === 'down'){
-   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
-   onChange(e);
-} else if (e.target.id === 'left'){
-   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
-   onChange(e);
-} else if (e.target.id === 'right'){
-  setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
-  onChange(e);
-}
-console.log(buttonDirection, grid, e, currentIndex);
+  function move(event) {
+    // e.target.id - direction variable
+    // getnext index function
+
+    const direction = event.target.id
+    const next = getNextIndex(direction);
+    //if next index /= index 
+
+    if (next != currentIndex){
+      setInitialSteps(initialSteps +1)
+      setInitialMessage(initialMessage)
+      setCurrentIndex(next)
+    } else {
+      setInitialMessage(`You can't go ${direction}`)
+    }
+
   }
 
 
@@ -136,14 +138,14 @@ console.log(buttonDirection, grid, e, currentIndex);
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message" >{initialMessage}</h3>
       </div>
       <div id="keypad">
-        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="left">LEFT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex) } id="up">UP</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="right">RIGHT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="down">DOWN</button>
-        <button id="reset" onClick={(e)=> reset()}>reset</button>
+        <button onClick={move} id="left">LEFT</button>
+        <button onClick={move} id="up">UP</button>
+        <button onClick={move} id="right">RIGHT</button>
+        <button onClick={move} id="down">DOWN</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
@@ -205,3 +207,21 @@ console.log(buttonDirection, grid, e, currentIndex);
     //   down: (currentIndex > 5) ? currentIndex : currentIndex + 3
     // }
   
+
+    //GETNECTINDEXPREVIOUS
+
+     // function getNextIndex(direction, grid, e, currentIndex) {
+  //   let coordinate = getXY(grid, currentIndex);
+  //   let userDirection = e.target.id 
+  //   let currentIndexLocation = grid.reduce((acc, block,index)  =>{ 
+  //     if (coordinate === block){
+  //       let data = {index:index, block:block}
+  //     return data;
+  //     }
+  //    return acc; 
+  // },{});
+  //     const nextIndex= direction[userDirection](currentIndexLocation.index)
+  //     setCurrentCoordinate(currentIndexLocation.block)
+  //     return nextIndex;
+    
+  // }
