dd37cf4104a0b7aa080858e060e5c6ea0c0a808d
Sabrina <Smg21022@gmail.com>
2023-06-30 06:45:02 -0700

    commented out some code not working

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react'
 
 export default function AppFunctional(props) {
@@ -6,28 +5,41 @@ export default function AppFunctional(props) {
 const [initialMessage, setInitialMessage] = useState('')
 const [initialEmail, setInitialEmail] = useState('')
 const [initialSteps, setInitialSteps] = useState(0)
-const [currentUserIndex, setcurrentUserIndex] = useState(0)
+const [currentIndex, setCurrentIndex] = useState(4)
 const [currentCoordinate, setCurrentCoordinate] = useState()
-const [error, setError] = useState()
-const[initialIndex, setInitialIndex] = useState(4)
+
+
+//coordinate array
 
   const grid = [
     [1,1],[1,2],[1,3],
     [2,1],[2,2],[2,3],
     [3,1],[3,2],[3,3]
   ]
+
+  //Setting which direction each button will move on the array above
  
   const buttonDirection = {
-    left: (index, block) => block.toString() !== "2,1" && block.toString() !== "1,1" && block.toString() !== "3,1"? index - 1 : index, 
-    right: (index, block) => block.toString() !== "1,3" && block.toString() !== "2,3" && block.toString() !== "3,3"? index + 1 : index,
-    up: (index, block) => block.toString() !== "1,1" && block.toString() !== "1,2" && block.toString() !== "1,3"? index - 3 : index,
-    down: (index, block) => block.toString() !== "3,1" && block.toString() !== "3,2" && block.toString() !== "3,3"? index + 3: index
+    left: (index) => index - 1,
+    right: (index) => index + 1,
+    up: (index) => index -3,
+    down: (index) => index + 3
   }
 
-function errorHandling (coordinates) {
-  console.log(`line 27`, coordinates)
- coordinates.toString() === "2,1" ? setError("You can't move left") : null;
-}
+  
+  // const buttonDirection = {
+  //   left: (index % 3 === 0) ? index : index - 1,
+  //   right: ((index -2) % 3 === 0) ? index : index + 1,
+  //   up: (index < 3) ? index :  index -3,
+  //   down: (index > 5) ? index : index + 3
+  // }
+
+  //For now I want to find a way to prevent the square from going back/reversed. This code is not working as it says index is not defined, and I tried Currentindex as well. 
+  //Why is the ternary code here not working, 
+
+ 
+
+//Gets the coordinate numbers, so it shows up on the users end. 
 
   function getXY(grid,idx) {
     const coordinatess = grid.find((block,index) => {
@@ -39,74 +51,69 @@ function errorHandling (coordinates) {
       return coordinatess;
   }
 
-  useEffect(()=> {
- if (initialSteps < 2){
-      setcurrentUserIndex(4)
- } 
-}, [initialSteps])
+  //To make it so it starts on loading with the B in the center, 
 
   useEffect(()=> {
-    
-    setCurrentCoordinate(getXY(grid, currentUserIndex))
-   
-  }, [currentUserIndex])
+    setCurrentCoordinate(getXY(grid, currentIndex))
+  }, [currentIndex])
   
+
+  //To reset everything
+
   function reset() {
     setInitialEmail('');
     setInitialSteps(0);
     setInitialMessage('');
-    setcurrentUserIndex(4);
+    setCurrentIndex(4);
+  
   }
 
-  function getNextIndex(direction, grid, e, currentUserIndex) {
-    let coordinate = getXY(grid, currentUserIndex);
+  //to let the computer know where the current location is 
 
+  function getNextIndex(direction, grid, e, currentIndex) {
+    let coordinate = getXY(grid, currentIndex);
     let userDirection = e.target.id 
-    console.log(`line 56`, userDirection)
-    let currentUserIndexLocation = grid.reduce((acc, block,index)  =>{ 
+    let currentIndexLocation = grid.reduce((acc, block,index)  =>{ 
       if (coordinate === block){
         let data = {index:index, block:block}
       return data;
       }
      return acc; 
   },{});
-      const nextIndex= direction[userDirection](currentUserIndex.index, currentUserIndexLocation.block)
-      console.log(`line 64 userIndex`, currentUserIndex)
-      console.log(`line 67 nextIndex`, nextIndex)
+      const nextIndex= direction[userDirection](currentIndexLocation.index)
+      setCurrentCoordinate(currentIndexLocation.block)
       return nextIndex;
-      
     
   }
+  
 
-  function move(buttonDirection, grid, e, currentUserIndex) {
-    if(e.target)
+
+  function move(buttonDirection, grid, e, currentIndex) {
 if (e.target.id === 'up'){
-  onChange(e);
-    setcurrentUserIndex(getNextIndex(buttonDirection, grid, e, currentUserIndex));
-    errorHandling(getXY(grid, currentUserIndex));
-console.log(`line 77`, getXY(grid, currentUserIndex))
-  } else if (e.target.id === 'down'){
-  onChange(e);
-   setcurrentUserIndex(getNextIndex(buttonDirection, grid, e, currentUserIndex));
-   errorHandling(getXY(grid, currentUserIndex))
+    setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
+    onChange(e);
+} else if (e.target.id === 'down'){
+   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
+   onChange(e);
 } else if (e.target.id === 'left'){
+   setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
+   onChange(e);
+} else if (e.target.id === 'right'){
+  setCurrentIndex(getNextIndex(buttonDirection, grid, e, currentIndex));
   onChange(e);
-   setcurrentUserIndex(getNextIndex(buttonDirection, grid, e, currentUserIndex));
-   errorHandling(getXY(grid, currentUserIndex))
-} else if (e.target.id === 'right'){ 
-  onChange(e);
-  setcurrentUserIndex(getNextIndex(buttonDirection, grid, e, currentUserIndex));
-  errorHandling(getXY(grid, currentUserIndex))
 }
+console.log(buttonDirection, grid, e, currentIndex);
   }
+
+
+
   function onChange(evt) {
-    if (initialSteps < 2){
-      setcurrentUserIndex(4)
-    }
-if (evt) {
+    if (evt) {
       setInitialSteps((prev) => prev+ 1)
-      setCurrentCoordinate(getXY(grid, currentUserIndex))
+      setCurrentCoordinate(getXY(grid, currentIndex))
+      console.log(`line 95`, currentCoordinate)
     }
+   
   }
 
   function onSubmit(evt) {
@@ -116,26 +123,26 @@ if (evt) {
   return (
     <div  id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{`Coordinates ${currentCoordinate}`}</h3>
+        <h3 id="coordinates">{currentIndex === 4 ?  `Coordinates ${currentCoordinate}`: `Coordinates ${currentCoordinate}`}</h3>
         <h3 id="steps">You moved {initialSteps} times</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === currentUserIndex ? ' active' : ''}`}>
-              {idx === currentUserIndex ? 'B' : null}
+            <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
+              {idx === currentIndex ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message">{error}</h3>
+        <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button onClick={(e) => move(buttonDirection, grid, e, currentUserIndex)} id="left">LEFT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentUserIndex)} id="up">UP</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentUserIndex)} id="right">RIGHT</button>
-        <button onClick={(e) => move(buttonDirection, grid, e, currentUserIndex)} id="down">DOWN</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="left">LEFT</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex) } id="up">UP</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="right">RIGHT</button>
+        <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="down">DOWN</button>
         <button id="reset" onClick={(e)=> reset()}>reset</button>
       </div>
       <form>
@@ -146,7 +153,55 @@ if (evt) {
   )
 }
 
+{/* <button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="left">LEFT</button>
+<button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="up">UP</button>
+<button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="right">RIGHT</button>
+<button onClick={(e) => move(buttonDirection, grid, e, currentIndex)} id="down">DOWN</button> */}
+
+
+// switch (direction) {
+//   case 'up':
+//     return (index < 3) ? index : index - 3
+//   case 'down':
+//     return (index > 5) ? index : index + 3
+//   case 'left':
+//     return (index % 3 === 0) ? index : index - 1
+//   case 'right':
+//     return ((index - 2) % 3 === 0) ? index : index + 1
+// }
 
 
 
 
+// function move(e, currentIndex) {
+//   direction = e.target.id;
+//   switch (direction) {
+//     case 'up':
+//       return (currentIndex < 3) ? currentIndex : currentIndex - 3
+//     case 'down':
+//       return (currentIndex > 5) ? currentIndex : currentIndex + 3
+//     case 'left':
+//       return (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1
+//     case 'right':
+//       return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1
+//   }
+// }
+
+ // switch (direction) {
+    //     case 'up':
+    //       return (currentIndex < 3) ? currentIndex : currentIndex - 3
+    //     case 'down':
+    //       return (currentIndex > 5) ? currentIndex : currentIndex + 3
+    //     case 'left':
+    //       return (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1
+    //     case 'right':
+    //       return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1
+    //   }
+
+    // const buttonDirection = {
+    //   left: (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1,
+    //   right: ((currentIndex -2) % 3 === 0) ? currentIndex : currentIndex + 1,
+    //   up: (currentIndex < 3) ? currentIndex :  currentIndex -3,
+    //   down: (currentIndex > 5) ? currentIndex : currentIndex + 3
+    // }
+  
