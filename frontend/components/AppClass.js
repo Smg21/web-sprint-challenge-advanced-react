5e6536936db1e704811154677d8ec50d096eb721
Sabrina <Smg21022@gmail.com>
2023-07-21 21:43:22 -0700

    changed teh grid/array so it is faster

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -6,37 +7,25 @@ class AppClass extends Component {
     super(props);
     this.state = {
       initialEmail: '',
-      initialMessage: '',
       initialSteps: 0,
       currentIndex: 4,
-      currentCoordinate: [],
+      initialMessage: '',
     };
 
-    this.grid = [
-      [1, 1], [1, 2], [1, 3],
-      [2, 1], [2, 2], [2, 3],
-      [3, 1], [3, 2], [3, 3]
-    ];
-  }
-
-  componentDidMount() {
-    this.setCurrentCoordinate();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.currentIndex !== prevState.currentIndex) {
-      this.setCurrentCoordinate();
-    }
+    // Faster and simpler grid representation
+    this.gridSize = 3;
   }
 
   setCurrentCoordinate() {
     const { currentIndex } = this.state;
-    const currentCoordinate = this.getXY(this.grid, currentIndex);
+    const currentCoordinate = this.getXY(this.gridSize, currentIndex);
     this.setState({ currentCoordinate });
   }
 
-  getXY(grid, idx) {
-    return grid.find((block, index) => index === idx);
+  getXY(gridSize, idx) {
+    const x = (idx % gridSize) + 1;
+    const y = Math.floor(idx / gridSize) + 1;
+    return [x, y];
   }
 
   handleChange = (e) => {
@@ -52,17 +41,18 @@ class AppClass extends Component {
     });
   }
 
-  getNextIndex(direction) {
+  getNextIndex = (direction) => {
     const { currentIndex } = this.state;
+    const gridSize = this.gridSize;
     switch (direction) {
       case 'up':
-        return (currentIndex < 3) ? currentIndex : currentIndex - 3;
+        return (currentIndex >= gridSize) ? currentIndex - gridSize : currentIndex;
       case 'down':
-        return (currentIndex > 5) ? currentIndex : currentIndex + 3;
+        return (currentIndex + gridSize < gridSize * gridSize) ? currentIndex + gridSize : currentIndex;
       case 'left':
-        return (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1;
+        return (currentIndex % gridSize !== 0) ? currentIndex - 1 : currentIndex;
       case 'right':
-        return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1;
+        return ((currentIndex + 1) % gridSize !== 0) ? currentIndex + 1 : currentIndex;
       default:
         return currentIndex;
     }
@@ -71,11 +61,9 @@ class AppClass extends Component {
   move = (event) => {
     const direction = event.target.id;
     const next = this.getNextIndex(direction);
-
     if (next !== this.state.currentIndex) {
       this.setState((prevState) => ({
         initialSteps: prevState.initialSteps + 1,
-        initialMessage: prevState.initialMessage,
         currentIndex: next
       }));
     } else {
@@ -85,8 +73,9 @@ class AppClass extends Component {
 
   onSubmit = (evt) => {
     evt.preventDefault();
-    const { currentCoordinate, initialEmail, initialSteps } = this.state;
-    const [x, y] = currentCoordinate;
+    
+    const { currentIndex, initialEmail, initialSteps } = this.state;
+    const [x, y] = this.getXY(this.gridSize, currentIndex);
 
     axios.post("http://localhost:9000/api/result", { email: initialEmail, steps: initialSteps, x, y })
       .then(res => {
@@ -102,17 +91,18 @@ class AppClass extends Component {
   }
 
   render() {
-    const { initialEmail, currentIndex, currentCoordinate, initialSteps, initialMessage } = this.state;
+    const { initialEmail, currentIndex, initialSteps, initialMessage } = this.state;
+    const currentCoordinate = this.getXY(this.gridSize, currentIndex);
 
     return (
       <div id="wrapper" className={this.props.className}>
         <div className="info">
-          <h3 id="coordinates">{currentIndex === 4 ? `Coordinates ${currentCoordinate}` : `Coordinates ${currentCoordinate}`}</h3>
+          <h3 id="coordinates">{`Coordinates (${currentCoordinate[0]}, ${currentCoordinate[1]})`}</h3>
           <h3 id="steps">You moved {initialSteps} times</h3>
         </div>
         <div id="grid">
           {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+            Array.from({ length: this.gridSize * this.gridSize }, (_, idx) => (
               <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
                 {idx === currentIndex ? 'B' : null}
               </div>
@@ -140,3 +130,170 @@ class AppClass extends Component {
 }
 
 export default AppClass;
+
+
+
+// import React, { Component } from 'react';
+// import axios from 'axios';
+
+// class AppClass extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       initialEmail: '',
+//       initialMessage: '',
+//       initialSteps: 0,
+//       currentIndex: 4,
+//       currentCoordinate: [],
+//     };
+
+//     this.grid = [
+//       [1, 1], [1, 2], [1, 3],
+//       [2, 1], [2, 2], [2, 3],
+//       [3, 1], [3, 2], [3, 3]
+//     ];
+
+//     //faster
+//     this.coordinateMap = new Map();
+//   }
+
+//   componentDidMount() {
+//     this.setCurrentCoordinate();
+//   }
+
+//   // componentDidUpdate(prevProps, prevState) {
+//   //   if (this.state.currentIndex !== prevState.currentIndex) {
+//   //     this.setCurrentCoordinate();
+//   //   }
+//   // }
+
+//   setCurrentCoordinate() {
+//     const { currentIndex } = this.state;
+//     const currentCoordinate = this.getXY(this.grid, currentIndex);
+//     this.setState({ currentCoordinate });
+//   }
+  
+// //faster
+// getXY(grid, idx) {
+//   if (this.coordinateMap.has(idx)) {
+//     return this.coordinateMap.get(idx);
+//   }
+
+//   const result = grid.find((block, index) => index === idx);
+//   this.coordinateMap.set(idx, result);
+//   return result;
+// }
+//   // getXY(grid, idx) {
+//   //   return grid.find((block, index) => index === idx);
+//   // }
+
+//   handleChange = (e) => {
+//     this.setState({ initialEmail: e.target.value });
+//   }
+
+//   reset = () => {
+//     this.setState({
+//       initialSteps: 0,
+//       initialMessage: '',
+//       currentIndex: 4,
+//       initialEmail: ''
+//     });
+//   }
+
+//   getNextIndex = (direction) => {
+//     const { currentIndex } = this.state;
+//     switch (direction) {
+//       case 'up':
+//         return (currentIndex < 3) ? currentIndex : currentIndex - 3;
+//       case 'down':
+//         return (currentIndex > 5) ? currentIndex : currentIndex + 3;
+//       case 'left':
+//         return (currentIndex % 3 === 0) ? currentIndex : currentIndex - 1;
+//       case 'right':
+//         return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1;
+//       default:
+//         return currentIndex;
+//     }
+//   }
+
+//   move = (event) => {
+//     const direction = event.target.id;
+//     const next = this.getNextIndex(direction);
+//     if (next !== this.state.currentIndex) {
+//       this.setState((prevState) => ({
+//         initialSteps: prevState.initialSteps + 1,
+//         currentIndex: next
+//       }));
+//     } else {
+//       // Display the message without updating the state
+//       this.setState({ initialMessage: `You can't go ${direction}` });
+//     }
+//     // if (next !== this.state.currentIndex) {
+//     //   this.setState((prevState) => ({
+//     //     initialSteps: prevState.initialSteps + 1,
+//     //     initialMessage: prevState.initialMessage,
+//     //     currentIndex: next
+//     //   }));
+//     // } else {
+//     //   this.setState({ initialMessage: `You can't go ${direction}` });
+//     // }
+//   }
+
+//   onSubmit = (evt) => {
+//     evt.preventDefault();
+    
+//     const { currentCoordinate, initialEmail, initialSteps } = this.state;
+//     const [x, y] = currentCoordinate;
+
+//     axios.post("http://localhost:9000/api/result", { email: initialEmail, steps: initialSteps, x, y })
+//       .then(res => {
+//         this.setState({ initialMessage: res.data.message });
+//       })
+//       .catch(err => {
+//         console.log(`currentcoordinate x`, x);
+//         console.log(`currentcoordinate y`, y);
+//         console.log(`initial Steps`, initialSteps);
+//         console.log(`initial Email`, initialEmail)
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     const { initialEmail, currentIndex, currentCoordinate, initialSteps, initialMessage } = this.state;
+
+//     return (
+//       <div id="wrapper" className={this.props.className}>
+//         <div className="info">
+//           <h3 id="coordinates">{currentIndex === 4 ? `Coordinates ${currentCoordinate}` : `Coordinates ${currentCoordinate}`}</h3>
+//           <h3 id="steps">You moved {initialSteps} times</h3>
+//         </div>
+//         <div id="grid">
+//           {
+//             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+//               <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
+//                 {idx === currentIndex ? 'B' : null}
+//               </div>
+//             ))
+//           }
+//         </div>
+//         <div className="info">
+//           <h3 id="message" >{initialMessage}</h3>
+//         </div>
+//         <div id="keypad">
+//           <button onClick={this.move} id="left">LEFT</button>
+//           <button onClick={this.move} id="up">UP</button>
+//           <button onClick={this.move} id="right">RIGHT</button>
+//           <button onClick={this.move} id="down">DOWN</button>
+//           <button id="reset" onClick={this.reset}>reset</button>
+//         </div>
+
+//         <form onSubmit={this.onSubmit}>
+//           <input value={initialEmail} id="email" type="email" placeholder="type email" onChange={this.handleChange}></input>
+//           <input id="submit" type="submit"></input>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default AppClass;
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,142 +1,96 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const AppFunctional = (props) => {
+  const gridSize = 3;
+  const [initialEmail, setInitialEmail] = useState('');
+  const [initialSteps, setInitialSteps] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(4);
+  const [initialMessage, setInitialMessage] = useState('');
+  const [currentCoordinate, setCurrentCoordinate] = useState([]);
 
- 
+  // Calculate currentCoordinate using useEffect
+  useEffect(() => {
+    const currentCoordinate = getXY(gridSize, currentIndex);
+    setCurrentCoordinate(currentCoordinate);
+  }, [currentIndex]);
 
-export default function AppFunctional(props) {
+  // Get X and Y coordinates based on grid size and index
+  const getXY = (gridSize, idx) => {
+    const x = (idx % gridSize) + 1;
+    const y = Math.floor(idx / gridSize) + 1;
+    return [x, y];
+  };
 
+  // Handle input change
+  const handleChange = (e) => {
+    setInitialEmail(e.target.value);
+  };
 
-
-// const [form, setForm] = useState( {email: ''});
-const [initialEmail, setInitialEmail] = useState('')
-const [initialMessage, setInitialMessage] = useState('')
-const [initialSteps, setInitialSteps] = useState(0)
-const [currentIndex, setCurrentIndex] = useState(4)
-const [currentCoordinate, setCurrentCoordinate] = useState()
-
-
-
-
-function handleChange (e){
-  setInitialEmail(e.target.value);
-}
-
-//coordinate array
-
-  const grid = [
-    [1,1],[1,2],[1,3],
-    [2,1],[2,2],[2,3],
-    [3,1],[3,2],[3,3]
-  ]
-
-
-  function getXY(grid,idx) {
-    const coordinatess = grid.find((block,index) => {
-      
-      if (index === idx){
-        return [block[0], block[1]];
-      }
-    })
-      return coordinatess;
-  }
-
-
-   // getXYMessage = () => {
-  //   // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-  //   // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-  //   // returns the fully constructed string.
-  // }
-  
-
-  //To make it so it starts on loading with the B in the center, 
-
-  useEffect(()=> {
-    setCurrentCoordinate(getXY(grid, currentIndex))
-  }, [currentIndex])
-  
-
-  //To reset everything
-
-  function reset() {
+  // Reset form values
+  const reset = () => {
     setInitialSteps(0);
     setInitialMessage('');
     setCurrentIndex(4);
     setInitialEmail('');
-  }
+  };
 
-  //to let the computer know where the current location is 
-
-  function getNextIndex(direction) {
+  // Get the next index based on direction
+  const getNextIndex = (direction) => {
     switch (direction) {
       case 'up':
-        return (currentIndex < 3) ? currentIndex : currentIndex - 3
+        return (currentIndex >= gridSize) ? currentIndex - gridSize : currentIndex;
       case 'down':
-        return (currentIndex > 5) ?currentIndex : currentIndex + 3
+        return (currentIndex + gridSize < gridSize * gridSize) ? currentIndex + gridSize : currentIndex;
       case 'left':
-        return (currentIndex % 3 === 0) ? currentIndex : currentIndex- 1
+        return (currentIndex % gridSize !== 0) ? currentIndex - 1 : currentIndex;
       case 'right':
-        return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1
+        return ((currentIndex + 1) % gridSize !== 0) ? currentIndex + 1 : currentIndex;
+      default:
+        return currentIndex;
     }
-    
-    
-  }
+  };
 
-
-
-  function move(event) {
-    // e.target.id - direction variable
-    // getnext index function
-
-    const direction = event.target.id
+  // Handle move button click
+  const move = (event) => {
+    const direction = event.target.id;
     const next = getNextIndex(direction);
-    //if next index /= index 
-
-    if (next != currentIndex){
-      setInitialSteps(initialSteps +1)
-      setInitialMessage(initialMessage)
-      setCurrentIndex(next)
+    if (next !== currentIndex) {
+      setInitialSteps(prevSteps => prevSteps + 1);
+      setCurrentIndex(next);
     } else {
-      setInitialMessage(`You can't go ${direction}`)
+      setInitialMessage(`You can't go ${direction}`);
     }
+  };
 
-  }
+  // Handle form submission
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    
+    const [x, y] = getXY(gridSize, currentIndex);
 
-
-  
-
-  function onSubmit(evt) {
-    evt.preventDefault()
-    let x = currentCoordinate[0] 
-    let y = currentCoordinate[1]
-    let email = initialEmail
-    let steps = initialSteps
- axios
-   .post("http://localhost:9000/api/result", {email, steps, x, y} )
-   .then(res => {
-     setInitialMessage(res.data.message); 
-   })
-   .catch(err => {
-    console.log(`currentcoordinate x`, x);
-    console.log(`currentcoordinate y`, y);
-    console.log(`initial Steps`, initialSteps);
-    console.log(`initial Email`, initialEmail)
-   
-     console.log(err); 
-   });
-   
-    // Use a POST request to send a payload to the server.
-  }
+    axios.post("http://localhost:9000/api/result", { email: initialEmail, steps: initialSteps, x, y })
+      .then(res => {
+        setInitialMessage(res.data.message);
+      })
+      .catch(err => {
+        console.log(`currentcoordinate x`, x);
+        console.log(`currentcoordinate y`, y);
+        console.log(`initial Steps`, initialSteps);
+        console.log(`initial Email`, initialEmail);
+        console.log(err);
+      });
+  };
 
   return (
-    <div  id="wrapper" className={props.className}>
+    <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{currentIndex === 4 ?  `Coordinates ${currentCoordinate}`: `Coordinates ${currentCoordinate}`}</h3>
+        <h3 id="coordinates">{`Coordinates (${currentCoordinate[0]}, ${currentCoordinate[1]})`}</h3>
         <h3 id="steps">You moved {initialSteps} times</h3>
       </div>
       <div id="grid">
         {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+          Array.from({ length: gridSize * gridSize }, (_, idx) => (
             <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
               {idx === currentIndex ? 'B' : null}
             </div>
@@ -155,26 +109,177 @@ function handleChange (e){
       </div>
 
       <form onSubmit={onSubmit}>
-        <input value={initialEmail} id="email" type="email" placeholder="type email" onChange={handleChange}  ></input>
+        <input value={initialEmail} id="email" type="email" placeholder="type email" onChange={handleChange}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
-}
+  );
+};
 
-//Trying To Figure Out How To Get Email To Work With Having The Value Of Whatever Is Typed In, 
-
-
+export default AppFunctional;
 
 
+// import React, { useState, useEffect } from 'react'
+// import axios from 'axios';
+
+
+ 
+
+// export default function AppFunctional(props) {
+
+
+
+// // const [form, setForm] = useState( {email: ''});
+// const [initialEmail, setInitialEmail] = useState('')
+// const [initialMessage, setInitialMessage] = useState('')
+// const [initialSteps, setInitialSteps] = useState(0)
+// const [currentIndex, setCurrentIndex] = useState(4)
+// const [currentCoordinate, setCurrentCoordinate] = useState()
 
 
 
 
-          {/* {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
-              </div>
-            ))
-          } */}
+// function handleChange (e){
+//   setInitialEmail(e.target.value);
+// }
+
+// //coordinate array
+
+//   const grid = [
+//     [1,1],[1,2],[1,3],
+//     [2,1],[2,2],[2,3],
+//     [3,1],[3,2],[3,3]
+//   ]
+
+
+//   function getXY(grid,idx) {
+//     const coordinatess = grid.find((block,index) => {
+      
+//       if (index === idx){
+//         return [block[0], block[1]];
+//       }
+//     })
+//       return coordinatess;
+//   }
+
+
+//    // getXYMessage = () => {
+//   //   // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
+//   //   // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
+//   //   // returns the fully constructed string.
+//   // }
+  
+
+//   //To make it so it starts on loading with the B in the center, 
+
+//   useEffect(()=> {
+//     setCurrentCoordinate(getXY(grid, currentIndex))
+//   }, [currentIndex])
+  
+
+//   //To reset everything
+
+//   function reset() {
+//     setInitialSteps(0);
+//     setInitialMessage('');
+//     setCurrentIndex(4);
+//     setInitialEmail('');
+//   }
+
+//   //to let the computer know where the current location is 
+
+//   function getNextIndex(direction) {
+//     switch (direction) {
+//       case 'up':
+//         return (currentIndex < 3) ? currentIndex : currentIndex - 3
+//       case 'down':
+//         return (currentIndex > 5) ?currentIndex : currentIndex + 3
+//       case 'left':
+//         return (currentIndex % 3 === 0) ? currentIndex : currentIndex- 1
+//       case 'right':
+//         return ((currentIndex - 2) % 3 === 0) ? currentIndex : currentIndex + 1
+//     }
+    
+    
+//   }
+
+
+
+//   function move(event) {
+//     // e.target.id - direction variable
+//     // getnext index function
+
+//     const direction = event.target.id
+//     const next = getNextIndex(direction);
+//     //if next index /= index 
+
+//     if (next != currentIndex){
+//       setInitialSteps(initialSteps +1)
+//       setInitialMessage(initialMessage)
+//       setCurrentIndex(next)
+//     } else {
+//       setInitialMessage(`You can't go ${direction}`)
+//     }
+
+//   }
+
+
+  
+
+//   function onSubmit(evt) {
+//     evt.preventDefault()
+//     let x = currentCoordinate[0] 
+//     let y = currentCoordinate[1]
+//     let email = initialEmail
+//     let steps = initialSteps
+//  axios
+//    .post("http://localhost:9000/api/result", {email, steps, x, y} )
+//    .then(res => {
+//      setInitialMessage(res.data.message); 
+//    })
+//    .catch(err => {
+//     console.log(`currentcoordinate x`, x);
+//     console.log(`currentcoordinate y`, y);
+//     console.log(`initial Steps`, initialSteps);
+//     console.log(`initial Email`, initialEmail)
+   
+//      console.log(err); 
+//    });
+   
+//     // Use a POST request to send a payload to the server.
+//   }
+
+//   return (
+//     <div  id="wrapper" className={props.className}>
+//       <div className="info">
+//         <h3 id="coordinates">{currentIndex === 4 ?  `Coordinates ${currentCoordinate}`: `Coordinates ${currentCoordinate}`}</h3>
+//         <h3 id="steps">You moved {initialSteps} times</h3>
+//       </div>
+//       <div id="grid">
+//         {
+//           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+//             <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
+//               {idx === currentIndex ? 'B' : null}
+//             </div>
+//           ))
+//         }
+//       </div>
+//       <div className="info">
+//         <h3 id="message" >{initialMessage}</h3>
+//       </div>
+//       <div id="keypad">
+//         <button onClick={move} id="left">LEFT</button>
+//         <button onClick={move} id="up">UP</button>
+//         <button onClick={move} id="right">RIGHT</button>
+//         <button onClick={move} id="down">DOWN</button>
+//         <button id="reset" onClick={reset}>reset</button>
+//       </div>
+
+//       <form onSubmit={onSubmit}>
+//         <input value={initialEmail} id="email" type="email" placeholder="type email" onChange={handleChange}  ></input>
+//         <input id="submit" type="submit"></input>
+//       </form>
+//     </div>
+//   )
+// }
+
